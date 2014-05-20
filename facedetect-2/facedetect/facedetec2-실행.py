00f93477c01dd192666c783af0eb5e1c91f6d7c6
noseungkuk <noesk622@naver.com>
2014-05-21 07:16:44 +0900

    semifinal

--- facedetect-2/facedetect/facedetec2-실행.py
+++ facedetect-2/facedetect/facedetec2-실행.py
@@ -4,8 +4,33 @@ import numpy as np
 import cv2
 import cv2.cv as cv
 from video import create_capture
+import math
 
+def Gaussian1D(GD, mean_a, mean_b, var, num):
+    temp[256]
 
+    for i in range(0,num):
+        if i < mean_a:
+            temp[i] = math.exp(-1*(i-mean_a) * (i-mean_a)/var)
+        elif (i >= mean_a) & (i <=mean_b):
+            temp[i] = 1.0
+        else:
+            temp[i] = math.exp(-1*(i-mean_a) * (i-mean_a)/var)
+    
+    min = 1.0
+    max = 0.0
+
+    for i in range(0, num):
+        if temp[i] < min:
+            min = temp[i]
+        if temp[i] > max:
+            max = temp[i]
+
+    mag = max - min
+
+    for i in range(0, num):
+        GD[i]=(((temp[i]-min)/mag)*255);
+ 
 def nothing(*arg):
     pass
 
@@ -25,8 +50,8 @@ def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
    
-        
-     
+
+
 cv2.namedWindow('facedetect')
 cv2.createTrackbar('off/on/start','facedetect',0,2,nothing)
 
@@ -68,9 +93,8 @@ if __name__ == '__main__':
         if (b == 2):
             #draw_rects(vis, rects, (0, 255, 0))
             if(len(rects) != 0):
-                cnt=0
                 avgpic=[0,0,0]
-
+                rgb=100000000
                 for i in range(rects[0][1], rects[0][3]):
                     for j in range(rects[0][0], rects[0][2]):
 
@@ -78,66 +102,46 @@ if __name__ == '__main__':
                         green=vis[i,j,1]
                         blue=vis[i,j,0]
                             
-                        #rgb=red+green+blue
-                        #print(red,green,blue,rgb)
-                           
-                        #print(vis[i,j])
+                        rgb=red+green+blue
+                        
+                        if rgb <= 10:
+                            vis[i,j] = 0
+                        else:
+                            if(red > 10):
+                                red=red*256/rgb
+                                #vis[i,j,2]=red
+                            else:
+                               vis[i,j,2]=0
+                            if(green > 10):
+                                green=green*256/rgb
+                                #vis[i,j,1]=green
+                            else:
+                                vis[i,j,1]=0
+                            if(blue > 10):
+                                blue=blue*256/rgb
+                                #vis[i,j,0]=blue
+                            else:
+                               vis[i,j,0]=0
+                            vis[i][j] = red * green / 255 ;
+                            #if(blue <100):
+                                #vis[i,j,1]=0
+                            '''
+                            if(green>150):
+                               vis[i,j,1]=255
+                            if(red > 100):
+                               vis[i,j]=255
+                            else:
+                               vis[i,j]=0
+                               '''
                         '''
-                        if(red > 0):
-                            red=red/rgb
-                            vis[i,j,2]=red
-                        else:
-                           vis[i,j,2]=0
-                        if(green > 0):
-                            green=green/rgb
-                            vis[i,j,1]=green
-                        else:
-                            vis[i,j,1]=0
-                        if(blue > 0):
-                            blue=blue/rgb
-                            vis[i,j,0]=blue
-                        else:
-                           vis[i,j,0]=0
-                           '''
-                        if(green<150):
-                           vis[i,j,1]=255
-                        if(red > 150):
-                           vis[i,j]=255
-                        else:
-                           vis[i,j]=0
-                           '''
                         if():
                             vis[i,j]=0
                             '''
                         #print(red+blue+green)
-                        
-                        '''
-                        avgpic[0]+=red
-                        avgpic[1]+=green
-                        avgpic[2]+=blue
-                        cnt+=1
-                        '''
+                       
                 #print("asdfasdfasdf")
-                '''
-                r=avgpic[0]/cnt
-                g=avgpic[1]/cnt
-                b=avgpic[2]/cnt
-
-                print(r,g,b)'''
-                '''
-                for x1, y1, x2, y2 in rects:
-                    for i in range(y1, y2):
-                        for j in range(x1, x2):
-                            if vis[i,j,0] < 1.1*avgpic[0]:
-                                if vis[i,j,0] > 0.9*avgpic[0]:
-                                    vis[i,j]=255
-                            if vis[i,j,1] < 1.1*avgpic[1]:
-                                if vis[i,j,1] > 0.9*avgpic[1]:
-                                    vis[i,j]=255
-                            if vis[i,j,2] < 1.1*avgpic[2]:
-                                if vis[i,j,2] > 0.9*avgpic[2]:
-                                    vis[i,j]=255
-                                    '''
+        
+        
         cv2.imshow('facedetect', vis)
         
         if 0xFF & cv2.waitKey(5) == 27:
